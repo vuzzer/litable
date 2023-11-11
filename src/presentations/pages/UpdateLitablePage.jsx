5911826ff70f8cbeb4ff68e22520dc97b94ccaf7
parfait kouassi <parfaitkouess@gmail.com>
2023-11-10 23:24:11 -0500

    implementing update fetching

--- src/presentations/pages/UpdateLitablePage.jsx
+++ src/presentations/pages/UpdateLitablePage.jsx
@@ -2,23 +2,47 @@
 
 import { useParams,  } from "react-router";
 import { getLitableById } from "../../data/litableData";
-
-export async function getLitableByIdImpl({ params }) {
-    const litable = await getLitableById(params.id)
-    return { litable }
-}
-
+import { useEffect, useState } from "react";
 
 
 export default function UpdateLitablePage() {
-    const { id } = useParams();
+    const [litable, setLitableItem] = useState(null)
+    useEffect(()=> {
+        //Get id of litable
+        const {id} = useParams();
+        getLitableById(id).then(({litable})=>{
+            setLitableItem(litable)
+        }).catch((e)=>{
+            const error = new Error()
+            error.message = "Error occured during fetching items"
+            throw error;
+        })
+
+    }, [])
     return (
         <div className="container">
             <h1>Update litable page</h1>
-            <h5>{id}</h5>
+            <h5>{litable}</h5>
         </div>
     );
 }
 
 
 
+//fetch litable based on ID
+function useLitable() {
+    const [litable, setLitableItem] = useState(null)
+    useEffect(()=> {
+        //Get id of litable
+        const {id} = useParams();
+        getLitableById(id).then(({litable})=>{
+            setLitableItem(litable)
+        }).catch((e)=>{
+            const error = new Error()
+            error.message = "Error occured during fetching items"
+            throw error;
+        })
+
+    }, [])
+    return litable
+}
